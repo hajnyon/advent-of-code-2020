a3adeb2aae06773b2252dcab2cc8e5fcdbf1bd3d
Ondra Hajný <insensaty@gmail.com>
2020-12-11 11:38:57 +0100

    feature: day 8

--- day-8/index.ts
+++ day-8/index.ts
@@ -3,54 +3,85 @@ import * as path from 'path';
 
 const REGEX: RegExp = /(?<operation>\w+) (?<sign>.)(?<value>\d+)/;
 
-function solve(data: string[]): number {
-    const instructions: {
-        operation: string;
-        value: number;
-        visited: boolean;
-    }[] = [];
+interface IInstruction {
+    operation: string;
+    value: number;
+}
 
-    for (const operation of data) {
-        const parse: RegExpExecArray = REGEX.exec(operation);
-        instructions.push({
-            operation: parse.groups.operation,
-            value: Number(parse.groups.value) * (parse.groups.sign === '+' ? 1 : -1),
-            visited: false
-        });
-    }
-
+function walkInstructions(instructions: IInstruction[]): { end: boolean; acc: number } {
     let acc: number = 0;
     let currentInstructionIndex: number = 0;
 
-    while (!instructions[currentInstructionIndex].visited) {
+    const visited: Set<number> = new Set();
+
+    while (!visited.has(currentInstructionIndex) && currentInstructionIndex < instructions.length) {
         const currentInstruction = instructions[currentInstructionIndex];
         switch (currentInstruction.operation) {
             case 'nop':
-                currentInstruction.visited = true;
+                visited.add(currentInstructionIndex);
                 currentInstructionIndex++;
                 break;
             case 'acc':
                 acc += currentInstruction.value;
-                currentInstruction.visited = true;
+                visited.add(currentInstructionIndex);
                 currentInstructionIndex++;
                 break;
             case 'jmp':
-                currentInstruction.visited = true;
+                visited.add(currentInstructionIndex);
                 currentInstructionIndex += currentInstruction.value;
                 break;
-
             default:
                 throw new Error(`Unknown operation '${currentInstruction.operation}'`);
         }
     }
 
-    return acc;
+    if (currentInstructionIndex === instructions.length) {
+        return { end: true, acc: acc };
+    }
+
+    return { end: false, acc: acc };
+}
+
+function swapInstruction(instruction: IInstruction) {
+    if (instruction.operation === 'nop') {
+        instruction.operation = 'jmp';
+    } else if (instruction.operation === 'jmp') {
+        instruction.operation = 'nop';
+    }
+}
+
+function solve(data: string[]): { lastAcc: number; endAcc: number } {
+    const instructions: IInstruction[] = [];
+
+    for (const operation of data) {
+        const parse: RegExpExecArray = REGEX.exec(operation);
+        instructions.push({
+            operation: parse.groups.operation,
+            value: Number(parse.groups.value) * (parse.groups.sign === '+' ? 1 : -1)
+        });
+    }
+
+    const lastAcc: number = walkInstructions(instructions).acc;
+
+    let endAcc: number;
+    for (const instruction of instructions) {
+        swapInstruction(instruction);
+        const result = walkInstructions(instructions);
+        if (result.end) {
+            endAcc = result.acc;
+            break;
+        }
+        swapInstruction(instruction);
+    }
+
+    return { lastAcc: lastAcc, endAcc: endAcc };
 }
 
 function main(): void {
     const data: string[] = fs.readFileSync(path.resolve(__dirname, 'input.txt'), { encoding: 'utf-8' }).split('\n');
-    const acc: number = solve(data);
-    console.log(`Last acc value: ${acc}`);
+    const result: { lastAcc: number; endAcc: number } = solve(data);
+    console.log(`Last acc value: ${result.lastAcc}`);
+    console.log(`End acc value: ${result.endAcc}`);
 }
 
 main();
